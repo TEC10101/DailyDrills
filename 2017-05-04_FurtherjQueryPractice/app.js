ecb993d6097cc1798b72c46c2caefd49051c26bd
Tyler Corum <tyler.corum@gmail.com>
2017-05-05 18:31:19 -0700

    playing with event handlers

--- 2017-05-04_FurtherjQueryPractice/app.js
+++ 2017-05-04_FurtherjQueryPractice/app.js
@@ -1,3 +1,35 @@
-$('li').on('click', function() {
-	$(this).text("Clicked!").delay(800).fadeOut(400);
+// @ts-check
+
+// Example to click and have a process happen, and chain events.
+// $('li').on('click', function() {
+// 	$(this).text("Clicked!").delay(800).fadeOut(400);
+// });
+
+// Element selectors
+/*
+$('li');
+$('a');
+$('img');
+$('p');
+
+
+    // Descendent selectors
+    $('p a');
+
+    // Classes and IDs
+    $('.class');
+    $('#ID');
+
+    // Attribute Selectors
+    $("[name='newsletter']"");
+*/
+
+console.log($('a').attr('href'));
+
+$('a').not('[href*="www.github.com"]').attr("target", "_blank");
+
+$('button').on('click', function () {
+  $('a').attr('href', 'http://www.github.com/');
+  console.log($('a').attr('href'));
 });
+
