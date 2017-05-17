9643fe2df742eed32e88a711fb1d6852b60b1c1d
Tyler Corum <tyler.corum@gmail.com>
2017-05-16 19:25:24 -0700

    starting to veer away form ObjOriented JS

--- 2017-05-13_quiz-challenge/app.js
+++ 2017-05-13_quiz-challenge/app.js
@@ -22,14 +22,17 @@ const progressElement = document.getElementById('progress');
 const choice0Element = document.getElementById('choice0');
 const choice1Element = document.getElementById('choice1');
 let answerLocation = RandomOrder();
+let otherLocation = 1-answerLocation;
 const answer0 = document.getElementById('guess0');
 const answer1 = document.getElementById('guess1');
 // let randomOther = RandomOther();
 
 if (answerLocation === 0){
-  Quiz.renderInElement(quizElement, progressElement, answer0);
+  // Display the multiple choice one way
+  Quiz.renderInElement(quizElement, progressElement, answer0, answer1);
 } else {
-  Quiz.renderInElement(quizElement, progressElement, answer1);
+  // ...display it the other way
+  Quiz.renderInElement(quizElement, progressElement, answer1, answer0);
 };
 
 
@@ -37,23 +40,28 @@ if (answerLocation === 0){
 
 
 answer0.onclick = function (){
-  Quiz.next();
-  Quiz.renderInElement(quizElement);
+  // If you answer correctly
+  if (answer0.textContent === Quiz.questions[Quiz.currentQuestion].answer.toString()){
+    console.log('Yay, testing correct answer');
+    // Then it reloads the div #quiz with next question
+    // Quiz.renderInElement(quizElement);
+  } else {
+    // Else it's incorrect and doesn't log correct count
+    console.log('Boo, wrong');
+  
+  }
 }
 
 
 
 answer1.onclick = function (){
-  Quiz.next();
-  Quiz.renderInElement(quizElement);
+  if (answer1.textContent === Quiz.questions[Quiz.currentQuestion].answer.toString()){
+    console.log('Yay, testing correct answer');
+    // Then it reloads the div #quiz with next question
+    //Quiz.renderInElement(quizElement);
+    // ...and logs to another variable how many are correct
+  } else {
+    // Else it's incorrect and doesn't log correct count
+    console.log('Boo, wrong');
+  }
 }
-
-
-/*
-const stopButton = document.getElementById('stop');
-stopButton.onclick = function (){
-  Playlist.stop();
-  Playlist.nowPlayingIndex = 0;
-  console.log('currentSong index is `' + Playlist.nowPlayingIndex + '`');
-  Playlist.renderInElement(quizElement);
-}*/
